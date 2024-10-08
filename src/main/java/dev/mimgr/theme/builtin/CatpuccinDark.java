e5d223be2f0060455089f3d4a25bc057229b0721
nd2204 <dn200422@gmail.com>
2024-10-08 17:51:24 +0700

    chores: update catpuccin colorscheme

--- src/main/java/dev/mimgr/theme/builtin/CatpuccinDark.java
+++ src/main/java/dev/mimgr/theme/builtin/CatpuccinDark.java
@@ -4,25 +4,25 @@ import java.awt.Color;
 
 public class CatpuccinDark extends ColorScheme {
   public CatpuccinDark() {
-    m_bg_dim = new Color(0x000000);
-    m_bg_0   = new Color(0x000000);
-    m_bg_1   = new Color(0x000000);
-    m_bg_2   = new Color(0x000000);
-    m_bg_3   = new Color(0x000000);
-    m_bg_4   = new Color(0x000000);
-    m_bg_5   = new Color(0x000000);
-    m_grey_0 = new Color(0x000000);
-    m_grey_1 = new Color(0x000000);
-    m_grey_2 = new Color(0x000000);
-    m_fg_0   = new Color(0x000000);
-    m_fg_1   = new Color(0x000000);
-    m_red    = new Color(0x000000);
-    m_orange = new Color(0x000000);
-    m_yellow = new Color(0x000000);
-    m_green  = new Color(0x000000);
-    m_aqua   = new Color(0x000000);
-    m_blue   = new Color(0x000000);
-    m_purple = new Color(0x000000);
-    m_accent = new Color(0x000000);
+    m_bg_dim = new Color(0x181926);
+    m_bg_0   = new Color(0x1e2030);
+    m_bg_1   = new Color(0x24273a);
+    m_bg_2   = new Color(0x24273a);
+    m_bg_3   = new Color(0x24273a);
+    m_bg_4   = new Color(0x24273a);
+    m_bg_5   = new Color(0x24273a);
+    m_grey_0 = new Color(0x363a4f);
+    m_grey_1 = new Color(0x363a4f);
+    m_grey_2 = new Color(0x494d64);
+    m_fg_0   = new Color(0xcad3f5);
+    m_fg_1   = new Color(0xa5adcb);
+    m_red    = new Color(0xed8796);
+    m_orange = new Color(0xf5a97f);
+    m_yellow = new Color(0xeed49f);
+    m_green  = new Color(0xa6da95);
+    m_aqua   = new Color(0xc6a0f6);
+    m_blue   = new Color(0x8aadf4);
+    m_purple = new Color(0xb7bdf8);
+    m_accent = m_green;
   }
 }
--- src/main/java/dev/mimgr/theme/builtin/CatpuccinLight.java
+++ src/main/java/dev/mimgr/theme/builtin/CatpuccinLight.java
@@ -4,25 +4,25 @@ import java.awt.Color;
 
 public class CatpuccinLight extends ColorScheme {
   public CatpuccinLight() {
-    m_bg_dim = new Color(0x000000);
-    m_bg_0   = new Color(0x000000);
-    m_bg_1   = new Color(0x000000);
-    m_bg_2   = new Color(0x000000);
-    m_bg_3   = new Color(0x000000);
-    m_bg_4   = new Color(0x000000);
-    m_bg_5   = new Color(0x000000);
-    m_grey_0 = new Color(0x000000);
-    m_grey_1 = new Color(0x000000);
-    m_grey_2 = new Color(0x000000);
-    m_fg_0   = new Color(0x000000);
-    m_fg_1   = new Color(0x000000);
-    m_red    = new Color(0x000000);
-    m_orange = new Color(0x000000);
-    m_yellow = new Color(0x000000);
-    m_green  = new Color(0x000000);
-    m_aqua   = new Color(0x000000);
-    m_blue   = new Color(0x000000);
-    m_purple = new Color(0x000000);
-    m_accent = new Color(0x000000);
+    m_bg_dim = new Color(0xdce0e8);
+    m_bg_0   = new Color(0xeff1f5);
+    m_bg_1   = new Color(0xe6e9ef);
+    m_bg_2   = new Color(0xccd0da);
+    m_bg_3   = new Color(0xbcc0cc);
+    m_bg_4   = new Color(0xacb0be);
+    m_bg_5   = new Color(0x9ca0b0);
+    m_grey_0 = new Color(0xbcc0cc);
+    m_grey_1 = new Color(0xacb0be);
+    m_grey_2 = new Color(0xbcc0cc);
+    m_fg_0   = new Color(0x4c4f69);
+    m_fg_1   = new Color(0x5c5f77);
+    m_red    = new Color(0xd20f39);
+    m_orange = new Color(0xfe640b);
+    m_yellow = new Color(0xdf8e1d);
+    m_green  = new Color(0x40a02b);
+    m_aqua   = new Color(0x179299);
+    m_blue   = new Color(0x1e66f5);
+    m_purple = new Color(0x7287fd);
+    m_accent = m_green;
   }
 }
