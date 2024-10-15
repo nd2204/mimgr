3693ef774e46b34ec24c0940c3e5406416ca5046
nd2204 <dn200422@gmail.com>
2024-10-15 11:04:04 +0700

    chores: clean up db connection logic

---
+++ src/main/java/dev/mimgr/db/DBConnection.java
@@ -0,0 +1,36 @@
+package dev.mimgr.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+  private static DBConnection instance = null;
+  private Connection connection;
+
+  private String url      = "jdbc:mysql://127.0.0.1:3306/mimgrdb?allowPublicKeyRetrieval=true&useSSL=true";
+  private String user     = "mimgr";
+  private String password = "mimgr-db";
+
+  private DBConnection() {
+    assert instance == null;
+    instance = new DBConnection();
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      connection = DriverManager.getConnection(url, user, password);
+    } catch (SQLException sqlex) {
+      System.out.println(sqlex);
+    } catch (Exception ex) {
+      System.out.println(ex);
+    }
+    assert connection != null;
+  }
+
+  public static synchronized DBConnection get_instance() {
+    return instance;
+  }
+
+  public Connection get_connection() {
+    return connection;
+  };
+}
