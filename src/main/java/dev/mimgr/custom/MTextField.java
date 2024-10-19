45acaf2f2cab5d8f3f075fb3024018e170bd539a
buidinhhuy <buidinhhuy900@gmail.com>
2024-10-19 10:37:49 +0700

    chores: update custom component

--- src/main/java/dev/mimgr/custom/MTextField.java
+++ src/main/java/dev/mimgr/custom/MTextField.java
@@ -4,22 +4,45 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Insets;
 import java.awt.RenderingHints;
 
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import dev.mimgr.theme.ColorTheme;
+import dev.mimgr.theme.ColorTheme.theme;
+import dev.mimgr.theme.builtin.ColorScheme;
 
 public class MTextField extends JTextField { 
-  private Color borderColor = Color.GRAY;
-  private int borderRadius = 0;
-  private float borderWidth = 1;
+  private ColorScheme colors;
+  private Color borderColor;
+  private Color focusBorderColor;
+  private int borderRadius = 15;
+  private int borderWidth = 1;
+  private Insets padding = new Insets(10, 10, 10, 10);
 
   // Constructor with default settings
   public MTextField(int columns) {
     super(columns);
-    setOpaque(false); // Make the background transparent for custom painting
-    setBorder(null);
+    Init();
   }
 
+  public MTextField() {
+    super();
+    Init();
+  }
+
+  private void Init() {
+    this.colors = ColorTheme.get_colorscheme(theme.THEME_LIGHT_DEFAULT);
+    padding.set(padding.top + borderWidth, padding.left + borderWidth, padding.bottom + borderWidth, padding.right + borderWidth);
+    this.setBorder(new EmptyBorder(padding));
+    this.setBorderColor(null);
+    this.borderColor = colors.m_fg_0;
+    this.focusBorderColor = colors.m_accent;
+    setOpaque(false); // Make the background transparent for custom painting
+  }
+  
   // Override the paintComponent method to customize the drawing
   @Override
   protected void paintComponent(Graphics g) {
@@ -30,17 +53,19 @@ public class MTextField extends JTextField {
 
     // Draw background
     g2d.setColor(getBackground());
-    g2d.fillRoundRect(0, 0, getWidth(), getHeight(), borderRadius, borderRadius);
+    g2d.fillRoundRect(borderWidth, borderWidth, getWidth() - (borderWidth * 2), getHeight() - (borderWidth * 2), borderRadius, borderRadius);
 
     // Draw the text
     super.paintComponent(g2d);
 
-    // Draw border
-    g2d.setColor(borderColor);
-    g2d.setStroke(new BasicStroke(borderWidth));
-    g2d.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, borderRadius, borderRadius);
+    g2d.setColor(this.borderColor);
+    if (isFocusOwner()) {
+      g2d.setColor(this.focusBorderColor);
+    }
+    g2d.setStroke(new BasicStroke(this.borderWidth));
+    g2d.drawRoundRect(borderWidth, borderWidth, getWidth() - (borderWidth * 2), getHeight() - (borderWidth * 2), borderRadius, borderRadius);
 
-    g2d.dispose(); // Dispose the graphics context to release system resources
+    g2d.dispose();
   }
 
   // Customization methods
@@ -55,7 +80,19 @@ public class MTextField extends JTextField {
   }
 
   public void setBorderWidth(int width) {
+    padding.set(padding.top + borderWidth, padding.left + borderWidth, padding.bottom + borderWidth, padding.right + borderWidth);
+    this.setBorder(new EmptyBorder(padding));
+
     this.borderWidth = width;
     repaint();
+  }
+
+  public void setPadding(int left, int top, int right, int bottom) {
+    padding.set(top + borderWidth, left + borderWidth, bottom + borderWidth, right + borderWidth);
+    this.setBorder(new EmptyBorder(padding));
+  }
+
+  public void setFocusBorderColor(Color color) {
+    this.focusBorderColor = color;
   }
 }
--- src/main/java/dev/mimgr/custom/RoundedPanel.java
+++ src/main/java/dev/mimgr/custom/RoundedPanel.java
@@ -1,23 +1,31 @@
 package dev.mimgr.custom;
 
-import javax.swing.*;
+import java.awt.BasicStroke;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JPanel;
 
 import dev.mimgr.theme.ColorTheme;
 import dev.mimgr.theme.ColorTheme.theme;
 import dev.mimgr.theme.builtin.ColorScheme;
-import java.awt.*;
 
 public class RoundedPanel extends JPanel {
-  private ColorScheme colors;
+  private final ColorScheme colors;
+  private int borderRadius = 15;
   private int borderWidth = 1;
 
   public RoundedPanel(ColorScheme colors) {
     this.colors = colors;
     setPreferredSize(new Dimension(200, 100));
+    this.setBackground(this.colors.m_bg_0);
   }
 
   public RoundedPanel() {
     colors = ColorTheme.get_colorscheme(theme.THEME_LIGHT_DEFAULT);
+    this.setBackground(colors.m_bg_0);
     setPreferredSize(new Dimension(200, 100));
     setOpaque(false);
   }
@@ -27,10 +35,20 @@ public class RoundedPanel extends JPanel {
     super.paintComponent(g);
     Graphics2D g2d = (Graphics2D) g;
     g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    g2d.setColor(colors.m_bg_0);
-    g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
-    g2d.setColor(colors.m_bg_0);
+    g2d.setColor(this.getBackground());
+    g2d.fillRoundRect(0, 0, getWidth(), getHeight(), this.borderRadius, this.borderRadius);
+    g2d.setColor(this.getBackground());
     g2d.setStroke(new BasicStroke(borderWidth));
-    g2d.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+    g2d.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, this.borderRadius, this.borderRadius);
+  }
+
+  public void setBorderRadius(int radius) {
+    this.borderRadius = radius;
+    repaint();
+  }
+
+  public void setBorderWidth(int width) {
+    this.borderRadius = width;
+    repaint();
   }
 }
