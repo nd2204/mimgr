291b69f090a51fc051bbe69b92095c64fc1e9dc5
buidinhhuy <buidinhhuy900@gmail.com>
2024-10-24 18:34:02 +0700

    Add: Colors

--- src/main/java/dev/mimgr/theme/builtin/EverforestDark.java
+++ src/main/java/dev/mimgr/theme/builtin/EverforestDark.java
@@ -4,25 +4,25 @@ import java.awt.Color;
 
 public class EverforestDark extends ColorScheme {
   public EverforestDark() {
-    m_bg_dim = new Color(0x000000);
-    m_bg_0   = new Color(0x000000);
-    m_bg_1   = new Color(0x000000);
-    m_bg_2   = new Color(0x000000);
-    m_bg_3   = new Color(0x000000);
-    m_bg_4   = new Color(0x000000);
-    m_bg_5   = new Color(0x000000);
-    m_grey_0 = new Color(0x000000);
-    m_grey_1 = new Color(0x000000);
-    m_grey_2 = new Color(0x000000);
-    m_fg_0   = new Color(0x000000);
-    m_fg_1   = new Color(0x000000);
-    m_red    = new Color(0x000000);
-    m_orange = new Color(0x000000);
-    m_yellow = new Color(0x000000);
-    m_green  = new Color(0x000000);
-    m_aqua   = new Color(0x000000);
-    m_blue   = new Color(0x000000);
-    m_purple = new Color(0x000000);
-    m_accent = new Color(0x000000);
+    m_bg_dim = new Color(#232A2E);
+    m_bg_0   = new Color(#2D353B);
+    m_bg_1   = new Color(#343F44);
+    m_bg_2   = new Color(#3D484D);
+    m_bg_3   = new Color(#475258);
+    m_bg_4   = new Color(#4F585E);
+    m_bg_5   = new Color(#56635F);
+    m_grey_0 = new Color(#7A8478);
+    m_grey_1 = new Color(#859289);
+    m_grey_2 = new Color(#9DA9A0);
+    m_fg_0   = new Color(#D3C5AA);
+    m_fg_1   = new Color(#D3C6AA);
+    m_red    = new Color(#E67E80);
+    m_orange = new Color(#E69875);
+    m_yellow = new Color(#DBBC7F);
+    m_green  = new Color(#A7C080);
+    m_aqua   = new Color(#83C092);
+    m_blue   = new Color(#7FBBB3);
+    m_purple = new Color(#D699B6);
+    m_accent = m_aqua;
   }
 }
--- src/main/java/dev/mimgr/theme/builtin/GruvboxDark.java
+++ src/main/java/dev/mimgr/theme/builtin/GruvboxDark.java
@@ -4,25 +4,25 @@ import java.awt.Color;
 
 public class GruvboxDark extends ColorScheme {
   public GruvboxDark() {
-    m_bg_dim = new Color(0x000000);
-    m_bg_0   = new Color(0x000000);
-    m_bg_1   = new Color(0x000000);
-    m_bg_2   = new Color(0x000000);
-    m_bg_3   = new Color(0x000000);
-    m_bg_4   = new Color(0x000000);
-    m_bg_5   = new Color(0x000000);
-    m_grey_0 = new Color(0x000000);
-    m_grey_1 = new Color(0x000000);
-    m_grey_2 = new Color(0x000000);
-    m_fg_0   = new Color(0x000000);
-    m_fg_1   = new Color(0x000000);
-    m_red    = new Color(0x000000);
-    m_orange = new Color(0x000000);
-    m_yellow = new Color(0x000000);
-    m_green  = new Color(0x000000);
-    m_aqua   = new Color(0x000000);
-    m_blue   = new Color(0x000000);
-    m_purple = new Color(0x000000);
-    m_accent = new Color(0x000000);
+    m_bg_dim = new Color(#1b1b1b);
+    m_bg_0   = new Color(#282828);
+    m_bg_1   = new Color(#32302f);
+    m_bg_2   = new Color(#323o2f);
+    m_bg_3   = new Color(#45403d);
+    m_bg_4   = new Color(#454o3d);
+    m_bg_5   = new Color(#5a524c);
+    m_grey_0 = new Color(#7c6f64);
+    m_grey_1 = new Color(#928374);
+    m_grey_2 = new Color(#a89984);
+    m_fg_0   = new Color(#d4be98);
+    m_fg_1   = new Color(#ddc7a1);
+    m_red    = new Color(#ea6962);
+    m_orange = new Color(#e78a4e);
+    m_yellow = new Color(#d8a657);
+    m_green  = new Color(#a9b665);
+    m_aqua   = new Color(#89b482);
+    m_blue   = new Color(#7daea3);
+    m_purple = new Color(#d3869b);
+    m_accent = m_aqua;
   }
 }
